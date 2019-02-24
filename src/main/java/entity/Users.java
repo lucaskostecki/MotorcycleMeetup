078bb275583b0a53392eb2d329a3732e27f053b0
Lucas Kostecki <lucas.kostecki@gmail.com>
2019-02-24 12:28:30 -0600

    Created Users table for MM. Created private instance variables for Users POJO
    Took 30 minutes

---
+++ src/main/java/entity/Users.java
@@ -0,0 +1,12 @@
+package entity;
+
+public class Users {
+
+    private int userID;
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int type;
+
+}
