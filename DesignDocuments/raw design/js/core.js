2971fe3af71e4e07842d0a6e96e904eb5c2e880f
Lucas Kostecki <lucas.kostecki@gmail.com>
2019-03-10 23:15:39 -0500

    Added homepage image for landing.

--- DesignDocuments/raw design/js/core.js
+++ DesignDocuments/raw design/js/core.js
@@ -1,9 +1,14 @@
 const init = () => {
+    // Get the items for nav
+    let optionsArrowButton = document.querySelector("#optionsArrowButton");
     let optionsArrow = document.querySelector("#optionsArrow");
     let optionsMenu = document.querySelector("#optionsMenu");
 
-    optionsArrow.addEventListener("click", function() {
+    // Add click event to arrow button
+    optionsArrowButton.addEventListener("click", function() {
+        // Add rotate class to arrow not button
         optionsArrow.classList.toggle("menuArrowRotate");
+        // Show the menu by adding class
         optionsMenu.classList.toggle("optionsMenuShow");
     });
 }
